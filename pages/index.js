f6d425a354b03704a73d4ca29478cff7011ea0f8
Mateus0808 <loiolamateus7@gmail.com>
2021-08-25 00:37:55 -0300

    feat: login form

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import React, { useState } from 'react'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+
 import {
   Container,
   Box,
@@ -6,18 +10,93 @@ import {
   Text,
   FormControl,
   FormLabel,
-  FormHelperText
+  FormHelperText,
+  InputGroup,
+  InputLeftAddon,
+  InputRightElement,
+  CheckIcon
 } from '@chakra-ui/react'
 
 import { Logo } from './../components'
 
+const validationSchema = yup.object().shape({
+  email: yup.string().email('E-mail inválido').required('Campo obrigatório'),
+  password: yup.string().required('Campo obrigatório').min(8, 'Requer pelo menos 8 caracteres'),
+  username: yup.string().required('Campo obrigatório')
+})
+
 function Home() {
+
+  const [ show, setShow ] = useState(false)
+
+  const { 
+    values, 
+    errors, 
+    touched, 
+    handleBlur, 
+    handleChange, 
+    handleSubmit,
+    isSubmitting
+  } = useFormik({
+    onSubmit: (values, form) => {
+      console.log(values)
+    },
+    validationSchema,
+    initialValues: {
+      email: '',
+      password: '',
+      username: ''
+    }
+  })
+
+  const handleClick = () => setShow(!show)
+
   return (
-    <Container>
+    <Container p={4} centerContent>
       <Logo />
-      <Box>
-        <Text></Text>
+      <Box p={4} mt={8}>
+        <Text>Crie sua agenda compartilhada</Text>
       </Box>
+
+      <Box>
+        <FormControl id="email" p={4} isRequired>
+            <FormLabel>Email</FormLabel>
+            <Input type="email" value={values.email} onChange={handleChange} onBlur={handleBlur}/>       
+            { touched.email && <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText> }
+        </FormControl>
+
+        <FormControl id="password" p={4} isRequired>
+          <FormLabel>Senha</FormLabel>
+          <InputGroup size="lg">
+            <Input 
+              pr="4.5rem"
+              type={ show ? "text" : "password" }
+              value={values.password} 
+              onChange={handleChange} 
+              onBlur={handleBlur}
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleClick}>
+                { show ? "Hide" : "Show" }
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          { touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText> }
+        </FormControl>
+        
+        <FormControl id="username" p={4} isRequired>
+          <InputGroup size="lg">
+            <InputLeftAddon children="clocker.work/" />
+            <Input type="username" value={values.username} onChange={handleChange} onBlur={handleBlur}/>       
+          </InputGroup>
+          { touched.username && <FormHelperText textColor="#e74c3c">{errors.username}</FormHelperText> }
+        </FormControl>
+
+        <Box p={4}>
+          <Button colorScheme="blue" width="100%" onClick={handleSubmit} isLoading={isSubmitting}>Entrar</Button>
+        </Box>
+      </Box>
+
     </Container>
   )
 }
