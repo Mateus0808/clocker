b006872159d49f07825909d58d6108dddd483921
Mateus0808 <loiolamateus7@gmail.com>
2021-08-24 22:41:47 -0300

    Added basic settings

---
+++ pages/agenda.js
@@ -0,0 +1,10 @@
+
+function Agenda() {
+  return (
+    <div>
+      Agenda
+    </div>
+  )
+}
+
+export default Agenda
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import {
+  Container,
+  Box,
+  Input,
+  Button,
+  Text,
+  FormControl,
+  FormLabel,
+  FormHelperText
+} from '@chakra-ui/react'
+
+import { Logo } from './../components'
+
+function Home() {
+  return (
+    <Container>
+      <Logo />
+      <Box>
+        <Text></Text>
+      </Box>
+    </Container>
+  )
+}
+
+export default Home
