4ac7436726b2bcc47b74912e55371e1fdb24ac9f
Mateus0808 <loiolamateus7@gmail.com>
2021-09-03 23:49:28 -0300

    feat: added phone mask

--- components/Input/index.js
+++ components/Input/index.js
@@ -1,12 +1,26 @@
-import { Input as InputBase } from "@chakra-ui/input";
-import { Box, FormControl, FormLabel, FormHelperText } from '@chakra-ui/react'
+import { mask, unMask} from 'remask'
+import { 
+  Input as InputBase, 
+  Box, 
+  FormControl, 
+  FormLabel, 
+  FormHelperText 
+} from '@chakra-ui/react'
 
-export const Input = ({ error, label, touched, ...props}) => (
-  <Box>
+export const Input = ({ error, label, touched, onChange, mask: pattern, ...props}) => {
+  const handleChange = event => {
+    const unMaskedValue = unMask(event.target.value)
+    const maskedValue = mask(unMaskedValue, pattern)
+
+    onChange && onChange(event.target.name)(maskedValue)
+  }
+
+
+  return (
     <FormControl id={props.name} p={4} isRequired>
       <FormLabel>{label}</FormLabel>
-      <InputBase {...props}/>
+      <InputBase {...props} onChange={ pattern ? handleChange: onChange }/>
       { touched && <FormHelperText textColor="#e74c3c">{error}</FormHelperText> }
     </FormControl>
-  </Box>
-)
+  )
+}
