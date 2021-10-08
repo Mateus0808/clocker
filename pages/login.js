abdfa1e4374ce02a40e5df115f34f6af0a1fbb0d
Mateus0808 <loiolamateus7@gmail.com>
2021-10-08 11:07:20 -0300

    login page complete

--- pages/login.js
+++ pages/login.js
@@ -1,105 +1,79 @@
-import React, { useState, useEffect } from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { useFormik } from 'formik'
 import * as yup from 'yup'
-import { useRouter } from 'next/router'
 
 import {
-  Container,
-  Box,
-  Input,
-  Button,
-  Text,
-  FormControl,
-  FormLabel,
-  FormHelperText,
-  InputGroup,
-  InputRightElement
+    Container,
+    Box,
+    Input,
+    Button,
+    Text,
+    FormControl,
+    FormLabel,
+    FormHelperText,
 } from '@chakra-ui/react'
 
-import { Logo, useAuth } from '../components'
+import { Logo, useAuth } from './../components'
 
 const validationSchema = yup.object().shape({
-  email: yup.string().email('E-mail inválido').required('Campo obrigatório'),
-  password: yup.string().required('Campo obrigatório').min(8, 'Requer pelo menos 8 caracteres')
+    email: yup.string().email('E-mail inválido').required('Preenchimento obrigatório'),
+    password: yup.string().required('Preenchimento obrigatório'),
 })
 
 export default function Login() {
-  const [ show, setShow ] = useState(false)
-  const [auth, { login }] = useAuth()
-  const router = useRouter()
+    const [auth, { login }] = useAuth()
+    const router = useRouter()
 
-  const { 
-    values, 
-    errors, 
-    touched, 
-    handleBlur, 
-    handleChange, 
-    handleSubmit,
-    isSubmitting
-  } = useFormik({
-    onSubmit: login,
-    validationSchema,
-    initialValues: {
-      email: '',
-      password: '',
-      username: ''
-    }
-  })
+    const {
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+    } = useFormik({
+        onSubmit: login,
+        validationSchema,
+        initialValues: {
+            email: '',
+            username: '',
+            password: ''
+        }
+    })
 
-  const handleClick = () => setShow(!show)
+    useEffect(() => {
+        auth.user && router.push('/agenda')
+    }, [auth.user])
 
-  useEffect(() => {
-    auth.user && router.push('/agenda')
-  }, [auth.user])
+    return (
+        <Container p={4} centerContent>
+            <Logo size={200} />
+            <Box p={4} mt={8}>
+                <Text>Crie sua agenda compartilhada</Text>
+            </Box>
 
-  return (
-    <Container p={4} centerContent>
-      <Logo size={320}/>
-      <Box p={4} mt={8}>
-        <Text>Crie sua agenda compartilhada</Text>
-      </Box>
+            <Box>
+                <FormControl id="email" p={4} isRequired>
+                    <FormLabel>Email</FormLabel>
+                    <Input size="lg" type="email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
+                    {touched.email && <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText>}
+                </FormControl>
 
-      <Box>
-        <FormControl id="email" p={4} isRequired>
-            <FormLabel>Email</FormLabel>
-            <Input 
-              type="email" 
-              value={values.email} 
-              onChange={handleChange} 
-              onBlur={handleBlur} 
-            />       
-            { touched.email && <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText> }
-        </FormControl>
+                <FormControl id="password" p={4} isRequired>
+                    <FormLabel>Senha</FormLabel>
+                    <Input size="lg" type="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+                    {touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText>}
+                </FormControl>
 
-        <FormControl id="password" p={4} isRequired>
-          <FormLabel>Senha</FormLabel>
-          <InputGroup size="lg">
-            <Input 
-              pr="4.5rem"
-              type={ show ? "text" : "password" }
-              value={values.password} 
-              onChange={handleChange} 
-              onBlur={handleBlur}
-            />
-            <InputRightElement width="4.5rem">
-              <Button h="1.75rem" size="sm" onClick={handleClick}>
-                { show ? "Hide" : "Show" }
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-          { touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText> }
-        </FormControl>
-        
-        <Box p={4}>
-          <Button colorScheme="blue" width="100%" onClick={handleSubmit} isLoading={isSubmitting}>Entrar</Button>
-        </Box>
-      </Box>
+                <Box p={4}>
+                    <Button colorScheme="blue" width="100%" onClick={handleSubmit} isLoading={isSubmitting}>Entrar</Button>
+                </Box>
+            </Box>
 
-      <Box display="flex" flexDir="row">
-        <FormLabel>Ainda não possui uma conta?</FormLabel>
-        <Link href="/signup">Cadastre-se</Link>
-      </Box>
-    </Container>
-  )
+            <Link href="/signup">Ainda não tem uma conta? Cadastre-se</Link>
+        </Container>
+    )
 }
